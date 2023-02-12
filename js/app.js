cb4f58560a363808667638f6ffcfbc2bb8d9fc98
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-02-11 17:23:06 -0800

    added class 3 lab

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let score = 0;
+
 let yourName = prompt('Welcome to my page! What is your name?');
 alert('Thanks ' + yourName + ', I\'m happy you\'re here! Let\'s play a guessing game to see how well you know me. Please respong with \'yes\' or \'no\'');
 
@@ -11,6 +13,8 @@ if (homeState.toLowerCase() === 'yes' || homeState.toLowerCase() === 'y') {
   alert('Wrong! I am originally from North Carolina.');
 } else if (homeState.toLowerCase() === 'no' || homeState.toLowerCase() === 'n' ) {
   alert('Correct! I am originally from North Carolina.');
+  score++;
+  console.log(score);
 } else {
   alert('Please respond with \'yes\' or \'no\' only!');
 }
@@ -21,6 +25,7 @@ let college = prompt('Do you think I graduated college?');
 
 if (college.toLowerCase() === 'yes' || college.toLowerCase() === 'y') {
   alert('Correct! I graduated from Appalachian State University.');
+  score++;
 } else if (college.toLowerCase() === 'no' || college.toLowerCase() === 'n') {
   alert('Wrong! I graduated from Appalachian State University.');
 } else {
@@ -35,6 +40,7 @@ if (cannabis.toLowerCase() === 'yes' || cannabis.toLowerCase() === 'y') {
   alert('Nope! I decided to move to Colorado and work in cannabis one month before graduating college.');
 } else if (cannabis.toLowerCase() === 'no' || cannabis.toLowerCase() === 'n') {
   alert('Correct! I decided to move to Colorado and work in cannabis one month before graduating college.');
+  score++;
 } else {
   alert('Please respond with \'yes\' or \'no\' only!');
 }
@@ -45,6 +51,7 @@ let travel = prompt('Do you think I enjoy travelling?');
 
 if (travel.toLowerCase() === 'yes'|| travel.toLowerCase() === 'y' ) {
   alert('Correct! I live to travel.');
+  score++;
 } else if (travel.toLowerCase() === 'no' || travel.toLowerCase() === 'n') {
   alert('Wow. It\'s like you don\'t even know me.');
 } else {
@@ -57,9 +64,63 @@ let goal = prompt('Do you think I will become a software engineer?');
 
 if (goal.toLowerCase() === 'yes' || goal.toLowerCase() === 'y') {
   alert('Me too! Thanks for believing in me friend.');
+  score++;
 } else if (goal.toLowerCase() === 'no' || goal.toLowerCase() === 'n') {
   alert('How rude!');
 }else {
   alert('Please respond with \'yes\' or \'no\' only!');
 }
 
+let myNumber = 7;
+let attempts = 3;
+let yourGuess = parseInt(prompt('I\'m thinking of a number between 1 and 10, can you guess what it is?'));
+// if (yourGuess === myNumber){
+//   alert ('You got it!');
+//   score++;
+// }
+while (myNumber != yourGuess) {
+  if (!attempts) {
+    alert ('You\'re out of guesses');
+    break;
+  }
+  if (yourGuess < myNumber) {
+    yourGuess = prompt('Too low! Guess again...');
+    attempts--;
+  } else if (yourGuess > myNumber) {
+    yourGuess = prompt('Too high! Guess again');
+    attempts--;
+  }
+}
+if (yourGuess == myNumber){
+  alert ('You got it!');
+  score++;
+}
+
+let places = ['Scotland', 'Ireland', 'Japan', 'Norway'];
+let guesses = 6;
+let userGuess = prompt('What is my #1 place I want to visit most?');
+let guessRight = false;
+
+while (guesses > 0) {
+  for (let i=0; i < places.length; i++) {
+    let currentplace = places[i];
+    // if (yourGuess.toLowerCase() !== currentplace.toLowerCase()) {
+    console.log (userGuess, currentplace);
+    if (userGuess.toLowerCase() === currentplace.toLowerCase()) {
+      alert(`You got it! I would love to go ${userGuess}`);
+      guessRight = true;
+      break; }
+  }
+  if (guessRight) break;
+
+  guesses = guesses-1;
+  if (!guesses) {
+    alert('You\'re out of guesses!');
+  }
+  else {
+    userGuess = prompt('I\'d love to go there but that\'s not my #1, guess again!');
+  }
+}
+
+alert (`You got a score of ${score}`);
+console.log(score);
